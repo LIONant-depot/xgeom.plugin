c92c40db79fde05b908d82990787ccaf07d4c180
LionAnt <nickreal03@gmail.com>
2025-10-24 11:01:23 +0800

    clean up

--- source/xgeom_xgpu_rsc_loader.h
+++ source/xgeom_xgpu_rsc_loader.h
@@ -7,19 +7,21 @@ namespace xrsc
 {
     // While this should be just a type... it also happens to be an instance... the instance of the texture_plugin
     // So while generating the type guid we must treat it as an instance.
-    inline static constexpr auto    geom_type_guid_v = xresource::type_guid(xresource::guid_generator::Instance64FromString("geom"));
-    using                           geom = xresource::def_guid<geom_type_guid_v>;
+    inline static constexpr auto    geom_type_guid_v    = xresource::type_guid(xresource::guid_generator::Instance64FromString("geom"));
+    using                           geom                = xresource::def_guid<geom_type_guid_v>;
 }
+
+struct xgeom;
 
 // Now we specify the loader and we must fill in all the information
 template<>
 struct xresource::loader< xrsc::geom_type_guid_v >
 {
     //--- Expected static parameters ---
-    constexpr static inline auto         type_name_v = L"geom";                      // This name is used to construct the path to the resource (if not provided)
-    using                                data_type = void;
-    static data_type* Load(xresource::mgr& Mgr, const full_guid& GUID);
-    static void                          Destroy(xresource::mgr& Mgr, data_type&& Data, const full_guid& GUID);
+    constexpr static inline auto            type_name_v = L"Geom";                      // This name is used to construct the path to the resource (if not provided)
+    using                                   data_type   = xgeom;
+    static data_type*                       Load        (xresource::mgr& Mgr, const full_guid& GUID);
+    static void                             Destroy     (xresource::mgr& Mgr, data_type&& Data, const full_guid& GUID);
 };
 
 #endif
